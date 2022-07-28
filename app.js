c6faa905ee7cd02273f840a0a3b86d3eb0356957
Roman <fniflheim@gmail.com>
2022-07-28 05:01:30 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+	const server = Hapi.server({
+		port: process.env.PORT || 5000,
+		host: 'localhost'
+	});
+
+	server.route([
+		{
+			method: 'GET',
+			path: '/',
+			handler: (request, h) => {
+				return 'Hello user';
+			}
+		}
+	]);
+
+	await server.start();
+	console.log('server running on %s', server.info.uri);
+
+};
+
+process.on('unhandledRejection', (err) => {
+	console.log(err);
+	process.exit(1);
+});
+
+init();
